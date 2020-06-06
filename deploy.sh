bb965d0529f0e20a89872b27f6474c9b8dd01cf8
Lawrence Baker <lbaker2@washcoll.edu>
2020-06-06 17:27:07 -0400

    point imperative updates to correct containers

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ docker push lbakerjs/scaling-octo-garbanzo-server:$GIT_SHA
 docker push lbakerjs/scaling-octo-garbanzo-worker:$GIT_SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment server=lbakerjs/scaling-octo-garbanzo-client:$GIT_SHA
+kubectl set image deployments/client-deployment client=lbakerjs/scaling-octo-garbanzo-client:$GIT_SHA
 kubectl set image deployments/server-deployment server=lbakerjs/scaling-octo-garbanzo-server:$GIT_SHA
-kubectl set image deployments/worker-deployment server=lbakerjs/scaling-octo-garbanzo-worker:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=lbakerjs/scaling-octo-garbanzo-worker:$GIT_SHA
