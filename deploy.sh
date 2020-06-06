6de9925a067fbdf2d2fac536448a73d3f82f900c
Lawrence Baker <lbaker2@washcoll.edu>
2020-06-06 16:57:59 -0400

    wrapping up deployment

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t lbakerjs/scaling-octo-garbanzo-client:latest -t lbakerjs/scaling-octo-garbanzo-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t lbakerjs/scaling-octo-garbanzo-server:latest -t lbakerjs/scaling-octo-garbanzo-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t lbakerjs/scaling-octo-garbanzo-worker:latest -t lbakerjs/scaling-octo-garbanzo-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker push lbakerjs/scaling-octo-garbanzo-client:latest
+docker push lbakerjs/scaling-octo-garbanzo-server:latest
+docker push lbakerjs/scaling-octo-garbanzo-worker:latest
+
+docker push lbakerjs/scaling-octo-garbanzo-client:$GIT_SHA
+docker push lbakerjs/scaling-octo-garbanzo-server:$GIT_SHA
+docker push lbakerjs/scaling-octo-garbanzo-worker:$GIT_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=lbakerjs/scaling-octo-garbanzo-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=lbakerjs/scaling-octo-garbanzo-server:$GIT_SHA
+kubectl set image deployments/worker-deployment server=lbakerjs/scaling-octo-garbanzo-worker:$GIT_SHA
